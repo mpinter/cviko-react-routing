aa1d37a0aef0501235e554b9a04c2bec2b20d76c
Martin Pinter <martin.pinter@vacuumlabs.com>
2021-03-27 19:25:42 +0100

    Naive routing 2: just state

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,30 @@ import Home from './components/Home';
 import About from './components/About';
 
 const App: React.FC = () => {
-    // naive approach 1 - does not preserve state, keeps reloading  the package
+    // naive approach 2 - just state, no url
+    const [route, setRoute] = useState<'Home' | 'About'>('Home');
     let whatToRender: string | React.ReactNode;
-    switch (window.location.pathname) {
-        case '/':
+    switch (route) {
+        case 'Home':
             whatToRender = <Home />;
             break;
-        case '/about':
+        case 'About':
             whatToRender = <About />;
             break;
         default:
             whatToRender = '404 - not found';
     }
-
     const [lightBgMode, setLightBgMode] = useState(false);
 
     return (
         <div className={lightBgMode ? 'App light' : 'App'}>
             <div>
-                <a href="/">Home </a>
-                <a href="/about">About</a>
+                <button type="button" onClick={() => setRoute('Home')}>
+                    Home
+                </button>
+                <button type="button" onClick={() => setRoute('About')}>
+                    About
+                </button>
             </div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
