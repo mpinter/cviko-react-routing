abcba1b36947fb4bd7981ec312d1466dc6c17ed8
Martin Pinter <martin.pinter@vacuumlabs.com>
2021-03-27 19:04:41 +0100

    Naive routing 1: hrefs

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
 
 const App: React.FC = () => {
+    // naive approach 1 - does not preserve state, keeps reloading  the package
+    let whatToRender: string | React.ReactNode;
+    switch (window.location.pathname) {
+        case '/':
+            whatToRender = <Home />;
+            break;
+        case '/about':
+            whatToRender = <About />;
+            break;
+        default:
+            whatToRender = '404 - not found';
+    }
+
+    const [lightBgMode, setLightBgMode] = useState(false);
+
     return (
-        <div className="App">
+        <div className={lightBgMode ? 'App light' : 'App'}>
+            <div>
+                <a href="/">Home </a>
+                <a href="/about">About</a>
+            </div>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                {whatToRender}
+                <button type="button" onClick={() => setLightBgMode(!lightBgMode)}>
+                    Toggle Background
+                </button>
             </header>
         </div>
     );
