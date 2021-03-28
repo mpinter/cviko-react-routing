969086c3919fb8253d43c4f0fadb4f2f44d3a4f8
Martin Pinter <martin.pinter@vacuumlabs.com>
2021-03-28 12:52:32 +0200

    Grpahql1: Apollo & Codegen setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
@@ -10,36 +10,35 @@ import GettingDataBetter from './components/GettingDataBetter';
 const App: React.FC = () => {
     const [lightBgMode, setLightBgMode] = useState(false);
     return (
-        <Router>
-            <div className={lightBgMode ? 'App light' : 'App'}>
-                <div className="header">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/data">Getting Data</Link>
-                    <Link to="/better-data">SWR</Link>
-                </div>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <Switch>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/data">
-                            <GettingData />
-                        </Route>
-                        <Route path="/better-data">
-                            <GettingDataBetter />
-                        </Route>
-                        <Route path="/">
-                            <Home />
-                        </Route>
-                    </Switch>
-                    <button type="button" onClick={() => setLightBgMode(!lightBgMode)}>
-                        Toggle Background
-                    </button>
-                </header>
+        <div className={lightBgMode ? 'App light' : 'App'}>
+            <div className="header">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/data">Getting Data</Link>
+                <Link to="/better-data">SWR</Link>
+                <Link to="/gql">GQL</Link>
             </div>
-        </Router>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/data">
+                        <GettingData />
+                    </Route>
+                    <Route path="/better-data">
+                        <GettingDataBetter />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+                <button type="button" onClick={() => setLightBgMode(!lightBgMode)}>
+                    Toggle Background
+                </button>
+            </header>
+        </div>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+    uri: 'https://api.spacex.land/graphql',
+    cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <Router>
+                <App />
+            </Router>
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
@@ -15,3 +26,17 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// client
+//     .query({
+//         query: gql`
+//             query PastLaunches {
+//                 launchesPast(limit: 10) {
+//                     mission_name
+//                     launch_date_local
+//                 }
+//             }
+//         `,
+//     })
+//     .then((result) => console.log(result))
+//     .catch((e) => console.log(e));
